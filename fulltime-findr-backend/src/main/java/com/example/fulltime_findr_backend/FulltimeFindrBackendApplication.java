9c2b364b396de116cf54e9e1698bfe32befd9999
SumanthJarvis <saisumanthreddy76@gmail.com>
2025-04-16 13:10:42 -0500

    Setup backend
    1. I setup the backend using spring initializer.io.
    2. Added dependencies like Spring Web, Spring Data JPA, PostgreSQL Driver, Spring Boot DevTools, Lombok, Validation, Spring Security, Flyway Migration, Spring actuator, Jsoup, and spring doc.
    3. Changed the folder name for front end.

---
+++ fulltime-findr-backend/src/main/java/com/example/fulltime_findr_backend/FulltimeFindrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fulltime_findr_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulltimeFindrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulltimeFindrBackendApplication.class, args);
+	}
+
+}
---
+++ fulltime-findr-backend/src/test/java/com/example/fulltime_findr_backend/FulltimeFindrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fulltime_findr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulltimeFindrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
